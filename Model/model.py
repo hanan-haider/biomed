e5f3624dd7c1d135c90145e3c402e07adde9d85e
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2025-10-04 19:22:32 +0500

    update

--- Model/model.py
+++ Model/model.py
@@ -3,7 +3,8 @@ from typing import Union, Tuple, Optional, Dict, Any
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .transformers import TimmModel, HFTextEncoder, TextTransformer
+from .transformers import TimmModel, HFTextEncoder
+
 import numpy as np
 
 # Import from local modules (commented out as requested)
