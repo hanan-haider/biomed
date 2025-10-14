b51959b67452c8ce4758f39e97c00b6febf46eb5
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2025-10-14 16:28:37 +0500

    update

--- utils.py
+++ utils.py
@@ -7,7 +7,11 @@ from transformers import AutoTokenizer
 
 # Initialize tokenizer (do this once, globally)
 tokenizer = AutoTokenizer.from_pretrained(
-    "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract"
+    "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract",
+    trust_remote_code=True,
+    use_fast=True,
+    # Explicitly ignore chat templates
+    chat_template=None
 )
 
 def encode_text_with_prompt_ensemble(model, obj, device):
